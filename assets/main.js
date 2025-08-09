a8973cf65c48ddb4f5bfbf5933991b11f2c75eb9
quyen <naundylan@gmail.com>
2025-08-09 21:09:38 +0700

    desktop menu

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@
 var navbar = document.querySelector('#navbar')
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
-    console.log(scrollPosition)
+    // console.log(scrollPosition)
     if(scrollPosition >= 100) {
         navbar.classList.add('fixedNavbar')
     }
@@ -10,3 +10,25 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('fixedNavbar')
     }
 })
+
+// Open & close menu
+var overlay = document.querySelector('.overlay');
+var dropdownMenu = document.querySelector('.dropdown-menu');
+var openBtn = document.querySelector('.open-btn');
+
+// console.log('Overlay:', overlay);
+// console.log('Dropdown Menu:', dropdownMenu);
+// console.log('Open Button:', openBtn);
+
+if (openBtn) {
+    openBtn.addEventListener('click', function() {
+        overlay.classList.remove('disabled');
+    });
+}
+
+if (overlay) {
+    overlay.addEventListener('click', function(e) {
+        dropdownMenu.classList.remove('show');
+        overlay.classList.add('disabled');
+    });
+}
