35ebc58228e52d853fd1b9b285ca30d27b75c5ee
quyen <naundylan@gmail.com>
2025-08-10 20:48:42 +0700

    responsive + mobile navbar

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,25 @@
-// Header
-var navbar = document.querySelector('#navbar')
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    // console.log(scrollPosition)
-    if(scrollPosition >= 100) {
-        navbar.classList.add('fixedNavbar')
-    }
-    else {
-        navbar.classList.remove('fixedNavbar')
-    }
-})
-
-// Open & close menu
+var navbar = document.querySelector('#navbar');
 var overlay = document.querySelector('.overlay');
 var dropdownMenu = document.querySelector('.dropdown-menu');
 var openBtn = document.querySelector('.open-btn');
+var closeBtn = document.querySelector('.close-btn')
 
-// console.log('Overlay:', overlay);
-// console.log('Dropdown Menu:', dropdownMenu);
-// console.log('Open Button:', openBtn);
+// Header
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    if(scrollPosition >= 100) {
+        navbar.classList.add('fixedNavbar');
+    } else {
+        navbar.classList.remove('fixedNavbar');
+    }
+});
 
+// Open & close menu
 if (openBtn) {
     openBtn.addEventListener('click', function() {
+        console.log('Button clicked')
         overlay.classList.remove('disabled');
+        dropdownMenu.classList.add('show');
     });
 }
 
@@ -31,4 +28,12 @@ if (overlay) {
         dropdownMenu.classList.remove('show');
         overlay.classList.add('disabled');
     });
+}
+
+// Close dropdown menu
+if (closeBtn) {
+    closeBtn.addEventListener('click', function() {
+        dropdownMenu.classList.remove('show');
+        overlay.classList.add('disabled')
+    })
 }
